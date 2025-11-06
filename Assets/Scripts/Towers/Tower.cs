7da7ab53d5e4e29ca6bccdc07b5532f2e96dd22d
Justin <justinstolk97@gmail.com>
2025-11-06 13:43:43 +0100

    Single tower shooting
    Tower is now an abstract base class, which specific tower types derive from.
    SingleTargetTower now shoots projectiles at enemies, which damages them

--- Assets/Scripts/Towers/Tower.cs
+++ Assets/Scripts/Towers/Tower.cs
@@ -2,27 +2,24 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum TowerAttackType { Single, Area, Multi }
 
-public class Tower : MonoBehaviour
+public abstract class Tower : MonoBehaviour
 {
-    [SerializeField, Tooltip("How much it costs to create this tower")] private ushort cost;
-    [SerializeField, Tooltip("The amount of damage dealt per hit")] private ushort power;
-    [SerializeField, Tooltip("The amount of time between hits")] private float attackInterval;
-    [SerializeField, Tooltip("The maximum range of the tower")] private float range;
-    // Consideration: towers with a minimum range?
-    [SerializeField, Tooltip("What kind of attack this tower has")] private TowerAttackType attackType;
-    // Conditional formatting should be added for area and multi target towers (or this has to be a inherited class?)
-    [SerializeField, Tooltip("Which layers are excluded?")] private LayerMask excludedLayers;
+    public ushort Cost => cost;
+    public ushort Power => power;
+    public float AttackInterval => attackInterval;
+    public Vector2 Range => range;
 
-    private List<Enemy> enemiesInRange;
+    protected abstract bool _hasTarget { get; }
+
+    [SerializeField, Tooltip("How much it costs to create this tower")] private ushort cost;
+    [SerializeField, Tooltip("The amount of damage dealt per hit")] protected ushort power;
+    [SerializeField, Tooltip("The amount of time between hits")] protected float attackInterval;
+    [SerializeField, Min(0), Tooltip("The range of the tower (min & max)")] protected Vector2 range = Vector2.up;
+    [SerializeField, Tooltip("Which layers are excluded?")] protected LayerMask excludedLayers;
+
     private float attackTimer;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        enemiesInRange = new();
-    }
 
     // Update is called once per frame
     void Update()
@@ -32,53 +29,25 @@ public class Tower : MonoBehaviour
         if (attackTimer > 0) return;
         // We cannot attack yet and thus, don't have to do anything.
 
-        Collider[] hits = Physics.OverlapSphere(transform.position, range, ~excludedLayers);
-        if (hits.Length == 0) return;
-        // There's no need to do any clearing or further calculations if nothing is in range.
+        FindTarget();
 
-        enemiesInRange.Clear();
-        foreach(Collider col in hits)
-        {
-            if(col.TryGetComponent(out Enemy enemy))
-            {
-                enemiesInRange.Add(enemy);
-                Debug.Log("Enemy with name of: " + enemy.name + " is in range of " + name);
-            }
-        }
-        if(enemiesInRange.Count > 0)
-        {
-            DetermineTarget(enemiesInRange);
-        }
+        if (!_hasTarget) return;
+
+        AttackTarget();
     }
 
-    private void DetermineTarget(List<Enemy> enemiesInRange)
+    protected virtual void AttackTarget()
     {
-        switch (attackType)
-        {
-            case TowerAttackType.Single:
-                IDamageable damageable = enemiesInRange[0] as IDamageable;
-                damageable.TakeDamage(power);
-                break;
-
-            case TowerAttackType.Area:
-                throw new NotImplementedException();
-                break;
-
-            case TowerAttackType.Multi:
-                foreach(Enemy enemy in enemiesInRange)
-                {
-                    IDamageable target = enemy as IDamageable;
-                    target.TakeDamage(power);
-                }
-                break;
-
-        }
         attackTimer = attackInterval;
     }
 
+    protected abstract void FindTarget();
+
     private void OnDrawGizmosSelected()
     {
+        Gizmos.color = Color.darkRed;
+        Gizmos.DrawWireSphere(transform.position, range.x);
         Gizmos.color = Color.aliceBlue;
-        Gizmos.DrawWireSphere(transform.position, range);
+        Gizmos.DrawWireSphere(transform.position, range.y);
     }
 }
